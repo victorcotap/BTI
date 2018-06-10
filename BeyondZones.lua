1247bbf548e6c8b04892a2bd693df70eda0c7515
victorcotap <levass.v@gmail.com>
2018-06-10 13:46:21 -0700

    More improvements and reporting

--- BeyondZones.lua
+++ BeyondZones.lua
@@ -16,7 +16,7 @@ ZonesList = {
 }
 
 HQ = GROUP:FindByName("BLUE CC")
-CommandCenter = COMMANDCENTER:New( HQ, "USA HQ" )
+CommandCenter = COMMANDCENTER:New( HQ, "HQ" )
 
 ZonesCaptureCoalition = {}
 
@@ -63,6 +63,7 @@ for keyIndex, zoneName in pairs(ZonesList) do
         self:E({Coalition = Coalition})
         if Coalition == coalition.side.BLUE then
             CommandCenter:MessageTypeToCoalition( string.format( "We captured %s, Excellent job!", ZoneCaptureCoalition:GetZoneName() ), MESSAGE.Type.Information )
+            ZoneCaptureCoalition:FlareZone( FLARECOLOR.White, 4 )
         else
             CommandCenter:MessageTypeToCoalition( string.format( "%s is captured by Russia, we lost it!", ZoneCaptureCoalition:GetZoneName() ), MESSAGE.Type.Information )
         end
@@ -84,13 +85,40 @@ function ZonesMarkingRefresh()
         local Zone = ZonesCaptureCoalition[keyIndex]
         Zone:Mark()
 
-        local Coalition = Zone:GetCoalition()
-        if Coalition == coalition.side.BLUE then
-            CommandCenter:MessageTypeToCoalition( string.format( "Zone %s is ours", ZonesCaptureCoalition[keyIndex]:GetZoneName() ), MESSAGE.Type.Information )
-        else
-            CommandCenter:MessageTypeToCoalition( string.format( "Zone %s is captured by the Russians", ZonesCaptureCoalition[keyIndex]:GetZoneName() ), MESSAGE.Type.Information )
-        end
+
     end
 end
 
 SCHEDULER:New(nil, ZonesMarkingRefresh, {}, 5, 30)
+
+function ZonesIntelRefresh()
+    env.info("BTI: Sending Intel to players ")
+    for keyIndex, zoneName in pairs(ZonesList) do
+        local Zone = ZonesCaptureCoalition[keyIndex]
+
+        local Coalition = Zone:GetCoalition()
+        if Coalition == coalition.side.BLUE then
+            if Zone:IsGuarded() then
+                CommandCenter:MessageTypeToCoalition( string.format( " %s is guarded by BLUFOR", ZonesCaptureCoalition[keyIndex]:GetZoneName() ), MESSAGE.Type.Information )
+            elseif Zone:IsAttacked() then
+                CommandCenter:MessageTypeToCoalition( string.format( " %s is attacked by REDFOR, go help!", ZonesCaptureCoalition[keyIndex]:GetZoneName() ), MESSAGE.Type.Information )
+            elseif Zone:IsEmpty() then
+                CommandCenter:MessageTypeToCoalition( string.format( " %s is BLUEFOR but empty", ZonesCaptureCoalition[keyIndex]:GetZoneName() ), MESSAGE.Type.Information )
+            elseif Zone:IsCaptured() then
+                CommandCenter:MessageTypeToCoalition( string.format( " %s is captured by BLUEFOR", ZonesCaptureCoalition[keyIndex]:GetZoneName() ), MESSAGE.Type.Information )
+            end
+        else
+            if Zone:IsGuarded() then
+                CommandCenter:MessageTypeToCoalition( string.format( " %s is captured by the Russians", ZonesCaptureCoalition[keyIndex]:GetZoneName() ), MESSAGE.Type.Information )
+            elseif Zone:IsAttacked() then
+                CommandCenter:MessageTypeToCoalition( string.format( " %s is attacked by BLUEFOR. Go help them!", ZonesCaptureCoalition[keyIndex]:GetZoneName() ), MESSAGE.Type.Information )
+            elseif Zone:IsEmpty() then
+                CommandCenter:MessageTypeToCoalition( string.format( " %s is empty! Go Capture it!", ZonesCaptureCoalition[keyIndex]:GetZoneName() ), MESSAGE.Type.Information )
+            elseif Zone:IsCaptured() then
+                CommandCenter:MessageTypeToCoalition( string.format( " %s is being captured by Russians! You lost it", ZonesCaptureCoalition[keyIndex]:GetZoneName() ), MESSAGE.Type.Information )
+            end
+        end
+    end
+end
+
+SCHEDULER:New(nil, ZonesIntelRefresh, {}, 20, 55)
