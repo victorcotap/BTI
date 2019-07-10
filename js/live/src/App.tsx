05a6d43b7b75a09485b2608e71e746f0071c7a72
victorcotap <levass.v@gmail.com>
2019-07-09 21:52:48 -0700

    Adds POC react app

---
+++ js/live/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+console.log("toto");
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header>
+        <a>Briefing </a>
+        <a>Map </a>
+        <a>About</a>
+      </header>
+        Toto
+    </div>
+  );
+}
+
+export default App;
