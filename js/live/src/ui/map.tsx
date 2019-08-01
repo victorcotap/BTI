46e8f098391af8aa24adeb2f11ce971925c00317
victorcotap <levass.v@gmail.com>
2019-08-01 00:01:25 -0700

    Adds Popup

--- js/live/src/ui/map.tsx
+++ js/live/src/ui/map.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import ReactMapboxGl from "react-mapbox-gl";
+import ReactMapboxGl, {Popup} from "react-mapbox-gl";
 
 import Group from '../model/group';
 import renderLayers from '../utils/layerRenderer';
+
+import './map.css';
 
 const Mapbox = ReactMapboxGl({
     accessToken: "pk.eyJ1IjoidmljdG9yY290YXAiLCJhIjoiY2p4eTdvZjRhMDdpejNtb2FmenRvenk0cCJ9.lf2sq-jELqUvTyPil0tWRA"
 });
 
 interface State {
-    currentGroups: Group[]
+    currentGroups: Group[],
+    selectedGroup?: Group,
 }
 
 export default class Map extends React.Component {
     state: State = {
-        currentGroups: Array<Group>()
+        currentGroups: Array<Group>(),
     }
 
 
@@ -40,6 +43,9 @@ export default class Map extends React.Component {
             console.log(error);
         }
     }
+    private groupClickHandler(group: Group) {
+        this.setState({selectedGroup: group})
+    }
 
     componentDidMount() {
         this.refreshData()
@@ -51,18 +57,29 @@ export default class Map extends React.Component {
                 <h2>Loading...</h2>
             )
         }
-        const layers = renderLayers(this.state.currentGroups);
+        const {selectedGroup} = this.state;
+        const layers = renderLayers(this.state.currentGroups, (group: Group) => this.groupClickHandler(group));
+        
         return (
             <div>
                 <h1>Here is the map</h1>
                 <Mapbox
                     style={"mapbox://styles/victorcotap/cjypbpdul4n6j1cmpkt13719b"}
-                    center={[42.262198, 42.721665]}
+                    center={selectedGroup ? [selectedGroup.longitude, selectedGroup.latitude] : [41.644793131899, 42.18450951825]}
                     containerStyle={{
-                        height: "100vh",
-                        width: "100vw"
+                        height: "80vh",
+                        width: "90vw"
                     }}>
                     {layers}
+                    {/* noop */}
+                    {selectedGroup ? <Popup key={selectedGroup.type} coordinates={[selectedGroup.longitude, selectedGroup.latitude]} offset={15}>
+                        <div className="PopupText">
+                            <h1>{selectedGroup.type}</h1>
+                            <span>Lon: {selectedGroup.longitude} Lat: {selectedGroup.latitude}</span><br />
+                            <span>{selectedGroup.height} meters</span><br />
+                            <span>{selectedGroup.heading} degrees</span>
+                        </div>
+                    </Popup> : null }
                 </Mapbox>
             </div>
         )
--- js/live/src/utils/layerRenderer.tsx
+++ js/live/src/utils/layerRenderer.tsx
@@ -5,16 +5,24 @@ import Group, { category, coalition } from '../model/group';
 import { Layer, Feature } from "react-mapbox-gl";
 
 
-function renderGroup(group: Group) {
+function renderGroup(group: Group, clickHandler: (group: Group) => void) {
     const key = `${group.type}${group.latitude}${group.longitude}`;
     return (
-        <Feature coordinates={[group.longitude, group.latitude]} properties={group} key={key} />
+        <Feature 
+            coordinates={[group.longitude, group.latitude]} 
+            properties={group} 
+            key={key}
+            onClick={(mapWithEvent) => {
+                    clickHandler(group);
+                }
+            }
+        />
     );
 }
 
-export default function renderLayers(groups: Group[]) {
+export default function renderLayers(groups: Group[], clickHandler: (group: Group) => void) {
     const redGroundGroups = groups.filter((group) => 
-        group.category === category.Ground && group.coalition === coalition.Red).map((group) => renderGroup(group));
+        group.category === category.Ground && group.coalition === coalition.Red).map((group) => renderGroup(group, clickHandler));
     const redGroundLayer = (<Layer
         key="redGroundLayer"
         type="symbol"
@@ -24,7 +32,7 @@ export default function renderLayers(groups: Group[]) {
     </Layer>)
     const unitsLayers = [redGroundLayer]
 
-    const redAirGroups = groups.filter((group) => group.category === category.Airplane && group.coalition === coalition.Red).map((group) => renderGroup(group));
+    const redAirGroups = groups.filter((group) => group.category === category.Airplane && group.coalition === coalition.Red).map((group) => renderGroup(group, clickHandler));
     const redAirLayer = (<Layer
         key="redAirLayer"
         type="symbol"
@@ -34,7 +42,7 @@ export default function renderLayers(groups: Group[]) {
     </Layer>)
     unitsLayers.push(redAirLayer)
 
-    const blueGroundGroups = groups.filter((group) => group.category === category.Ground && group.coalition === coalition.Blue).map((group) => renderGroup(group));
+    const blueGroundGroups = groups.filter((group) => group.category === category.Ground && group.coalition === coalition.Blue).map((group) => renderGroup(group, clickHandler));
     const blueGroundLayer = (<Layer
         key="blueGroundLayer"
         type="symbol"
@@ -44,7 +52,7 @@ export default function renderLayers(groups: Group[]) {
     </Layer>)
     unitsLayers.push(blueGroundLayer)
 
-    const blueAirGroups = groups.filter((group) => group.category === category.Airplane && group.coalition === coalition.Blue).map((group) => renderGroup(group));
+    const blueAirGroups = groups.filter((group) => group.category === category.Airplane && group.coalition === coalition.Blue).map((group) => renderGroup(group, clickHandler));
     const blueAirLayer = (<Layer
         key="blueAirLayer"
         type="symbol"
@@ -54,7 +62,7 @@ export default function renderLayers(groups: Group[]) {
     </Layer>)
     unitsLayers.push(blueAirLayer);
 
-    const blueShipGroups = groups.filter((group) => group.category === category.Ship && group.coalition === coalition.Blue).map((group) => renderGroup(group));
+    const blueShipGroups = groups.filter((group) => group.category === category.Ship && group.coalition === coalition.Blue).map((group) => renderGroup(group, clickHandler));
     const blueShipLayer = (<Layer
         key="blueShipLayer"
         type="symbol"
