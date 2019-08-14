92023be89ed7a3651972d7c2316b3c8220b249b0
victorcotap <levass.v@gmail.com>
2019-08-13 21:47:14 -0700

    console info for core

--- js/core/app/liveData.ts
+++ js/core/app/liveData.ts
@@ -1,19 +1,21 @@
 import express from 'express';
 
-import TrackingStore from './trackingStore'
+import TrackingStore from './trackingStore';
 
 const router = express.Router();
 const cacheTime = 60;
-const trackingFilePath = "/BTI/TrackingFile.json"
+const trackingFilePath = "/BTI/TrackingFile.json";
 
 const store = new TrackingStore(trackingFilePath);
 
 router.get('/', async (request, response) => {
-    const currentTime = new Date()
+    const currentTime = new Date();
     if (currentTime.getTime() - store.cache.time.getTime() > 30000) {
-        await store.readTrackingFile()
+        console.info('Live data cache is stale, refreshing');
+        await store.readTrackingFile();
     }
 
+    console.log('Live data access');
     response.json({currentGroups: store.cache.currentGroups});
 });
 
