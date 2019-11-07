27299c57ca57eaad55b20946d74278946dc89f88
victorcotap <levass.v@gmail.com>
2019-11-07 14:03:01 -0800

    Implements basic Discord airboss bot

--- js/core/stores/airbossStore.ts
+++ js/core/stores/airbossStore.ts
@@ -1,12 +1,21 @@
 import * as fs from 'fs';
 import parse from 'csv-parse/lib/sync';
+import Discord from 'discord.js';
+
 import { promisify } from 'util';
 import { delimiter } from 'path';
+
+import config from '../config.json';
 
 import Trap, {trapFromCSVEntry} from '../model/trap';
 
 const readFile = promisify(fs.readFile);
-
+const client = new Discord.Client()
+let channel: Discord.Channel | undefined;
+client.on('ready', () => {
+    channel = client.channels.get(config.DiscordChannelID);
+});
+client.login(config.DiscordToken);
 
 export interface Cache {
     time: Date,
@@ -22,17 +31,47 @@ export default class AirbossStore {
 
     constructor(filePath: string) {
         this.filePath = filePath;
-        this.readTrackingFile()
+        this.readTrackingFile(true)
+        setInterval(() => this.readTrackingFile(false), 30000);
     }
 
-    async readTrackingFile() {
+    private async  sendNewTrapToDiscord(trap: Trap) {
+        if (channel) {
+            const embed = new Discord.RichEmbed();
+            embed.setTitle('New entry on the Greenie Board');
+            embed.setDescription("Here is what the LSO has to say");
+            embed.setFooter('Provided by MiniBoss');
+            embed.addField("Pilot Name", trap.pilotName, true);
+            embed.addField("Airframe", trap.airframe, true);
+            embed.addField("Points", trap.points, true);
+            embed.addField('Grade', trap.grade, true);
+            embed.addField('Details', trap.detail, true);
+            embed.addField('Wire', trap.wire, true);
+            embed.addField('Time in groove', trap.timeGroove, true);
+            embed.addField('Wind', trap.wind, true);
+
+            try {
+                await channel.send('OHAI!', {embed});
+            } catch (error) {
+                console.warn(error);
+            }
+        }
+    }
+
+    async readTrackingFile(firstTime: boolean) {
         try {
             const buffer = await readFile(this.filePath, {encoding: 'utf-8'})
             const csv = parse(buffer, {skip_empty_lines: true, columns: true})
-            this.cache.currentTraps = csv.map((entry: Object) => trapFromCSVEntry(entry));
+            const newTraps: Trap[] = csv.map((entry: Object) => trapFromCSVEntry(entry));
+            //DEBUG add first time protection again
+            // this.sendNewTrapToDiscord(newTraps[newTraps.length - 1]);
+            if (!firstTime && this.cache.currentTraps !== newTraps) {
+                this.sendNewTrapToDiscord(newTraps[newTraps.length - 1]);
+            }
+            this.cache.currentTraps = newTraps;
             this.cache.time = new Date();
         } catch (error) {
-            console.log(`Unable to read file ${this.filePath} error`, error);
+            console.warn(`Unable to read file ${this.filePath} error`, error);
         }
     }
 }
