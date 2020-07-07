64cb4d461b4d89a985dd7396a2c04491f378e09f
victorcotap <levass.v@gmail.com>
2020-07-06 18:58:12 -0700

    Switching from Moose to DCS api

--- TOP/TOPPersistence.lua
+++ TOP/TOPPersistence.lua
@@ -1,7 +1,7 @@
 env.info("TOP: Persistence here! OHAI")
 
 if JSONLib == nil then
-  JSONLib = dofile("C:\\BTI\\Json.lua")
+    JSONLib = dofile("C:\\BTI\\Json.lua")
 end
 
 local persistenceMaster = {}
@@ -12,44 +12,60 @@ SetPersistenceGroups = SET_GROUP:New():FilterActive():FilterCoalitions("red"):Fi
 local function trackGroup(group, master)
     local groupName = group.GroupName
     local groupCoalition = group:GetCoalition()
-    local groupAlive = group:IsAlive()
+    local groupData = persistenceMaster[groupName]
 
-    if groupName then
-        -- env.info("TOP: tracking group data " .. groupName .. " -> " .. UTILS.OneLineSerialize({groupCoalition, groupName, groupCategory, groupType, groupAlive}))
-        local groupData = persistenceMaster[groupName]
-        local groupIsReallyAlive = groupAlive
-        if groupData ~= nil and groupData["alive"] == false then
-            groupIsReallyAlive = false
-            persistenceMaster[groupName] = {
-                ["alive"] = groupIsReallyAlive,
-                ["coalition"] = groupCoalition,
-            }
-            return
-        end
-
-        local dcsGroup = GROUP:FindByName(groupName)
-
-        if dcsGroup ~= nil then
-            local groupUnits = dcsGroup:GetUnits()
-            if groupUnits == nil or #groupUnits == 0 then
-                env.info("TOP: Persistence can't find units marking group " .. groupName .. " as dead")
-                groupIsReallyAlive = false
-            end
-        else
-            env.info("TOP: Persistence can't find group, marking group " .. groupName .. "  as dead")
-            groupIsReallyAlive = false
-        end
-
-        persistenceMaster[groupName] = {
-            ["alive"] = groupIsReallyAlive,
-            ["coalition"] = groupCoalition,
-        }
+    -- Protect against a group coming back to life (thanks ED)
+    if groupData ~= nil and groupData["alive"] == false then
+        env.info("TOP: Group is already marked as dead, skipping dead detection")
+        return
     end
+
+    local dcsGroup = Group:getByName(groupName)
+    local groupAlive = group:IsAlive()
+    env.info("TOP: MOOSE IsAlive() " .. groupName .. " " .. tostring(groupAlive))
+
+    if dcsGroup then
+        if dcsGroup and dcsGroup:isExist() and dcsGroup:getSize() > 0 then
+            env.info("TOP: Group " .. groupName .. " is considered alive")
+            groupAlive = true
+        else
+            env.info("TOP: Persistence can't find units marking group " .. groupName .. " as dead")
+            groupAlive = false
+        end
+    else
+        groupAlive = false
+        env.info("TOP: Persistence can't find group, marking group " .. groupName .. "  as dead")
+    end
+
+    persistenceMaster[groupName] = {
+        ["alive"] = groupAlive,
+        ["coalition"] = groupCoalition
+    }
+
+    -- if groupName then
+    --     -- env.info("TOP: tracking group data " .. groupName .. " -> " .. UTILS.OneLineSerialize({groupCoalition, groupName, groupCategory, groupType, groupAlive}))
+
+    --     if dcsGroup ~= nil then
+    --         local groupUnits = dcsGroup:getUnits()
+    --         if groupUnits == nil or #groupUnits < 1 then
+    --             env.info("TOP: Persistence can't find units marking group " .. groupName .. " as dead")
+    --             groupIsReallyAlive = false
+    --         end
+    --     else
+    --         env.info("TOP: Persistence can't find group, marking group " .. groupName .. "  as dead")
+    --         groupIsReallyAlive = false
+    --     end
+
+    --     persistenceMaster[groupName] = {
+    --         ["alive"] = groupIsReallyAlive,
+    --         ["coalition"] = groupCoalition
+    --     }
+    -- end
 end
 
 function trackPersistenceGroups()
     SetPersistenceGroups:ForEachGroup(
-        function (group)
+        function(group)
             trackGroup(group, persistenceMaster)
         end
     )
@@ -57,43 +73,43 @@ function trackPersistenceGroups()
 end
 
 local function applyMaster(master)
-  env.info("TOP: Persistence is applying master")
-  for groupName, group in pairs(master) do
-      local persistedGroup = master[groupName]
-      local dcsGroup = GROUP:FindByName(groupName)
-      if dcsGroup ~= nil then
-          if group["alive"] == nil or group["alive"] == false then
-              env.info("TOP: Destroying dead group" .. groupName)
-              dcsGroup:Destroy()
-          end
-      elseif group["alive"] == nil or group["alive"] == false then
-          env.info("TOP: Couldn't find dead group " .. groupName .. "to apply master to")
-      end
-  end
+    env.info("TOP: Persistence is applying master")
+    for groupName, group in pairs(master) do
+        local persistedGroup = master[groupName]
+        local dcsGroup = GROUP:FindByName(groupName)
+        if dcsGroup ~= nil then
+            if group["alive"] == nil or group["alive"] == false then
+                env.info("TOP: Destroying dead group" .. groupName)
+                dcsGroup:Destroy()
+            end
+        elseif group["alive"] == nil or group["alive"] == false then
+            env.info("TOP: Couldn't find dead group " .. groupName .. "to apply master to")
+        end
+    end
 end
 
 function saveMasterPersistence(master, masterPath)
-  if master == nil then
-      env.info("TOP: No master provided for saving")
-      return
-  end
-  local newMasterJSON = JSONLib.encode(master)
-  saveFile(masterPath, newMasterJSON)
+    if master == nil then
+        env.info("TOP: No master provided for saving")
+        return
+    end
+    local newMasterJSON = JSONLib.encode(master)
+    saveFile(masterPath, newMasterJSON)
 end
 
 -- Tracking Engine --------------------------------------------------------
 function startPersistenceEngine(something)
-  local savedMasterBuffer = loadFile(persistenceMasterPath)
-  if savedMasterBuffer ~= nil and TOPGroupPersistence then
-      local savedMaster = JSONLib.decode(savedMasterBuffer)
-      applyMaster(savedMaster)
-      persistenceMaster = savedMaster
-  else
-      env.info("TOP: No Tracking master file found, reset in progress")
-  end
-  SCHEDULER:New(nil, trackPersistenceGroups, {"something"}, 10, 60)
+    local savedMasterBuffer = loadFile(persistenceMasterPath)
+    if savedMasterBuffer ~= nil and TOPGroupPersistence then
+        local savedMaster = JSONLib.decode(savedMasterBuffer)
+        applyMaster(savedMaster)
+        persistenceMaster = savedMaster
+    else
+        env.info("TOP: No Tracking master file found, reset in progress")
+    end
+    SCHEDULER:New(nil, trackPersistenceGroups, {"something"}, 10, 60)
 
-  SCHEDULER:New(nil, saveMasterPersistence, {persistenceMaster, persistenceMasterPath}, 30, 60)
+    SCHEDULER:New(nil, saveMasterPersistence, {persistenceMaster, persistenceMasterPath}, 30, 60)
 end
 
 SCHEDULER:New(nil, startPersistenceEngine, {trackingMaster, trackingMasterPath}, 10)
