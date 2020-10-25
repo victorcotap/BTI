34ee2ba72130a9f56203be9a1972f66aa409043c
victorcotap <levass.v@gmail.com>
2020-10-25 12:22:42 -0700

    Logbook addition to TOPSlots

--- TOP/TOPSlots-Hook.lua
+++ TOP/TOPSlots-Hook.lua
@@ -3,7 +3,8 @@ local lfs = require("lfs")
 
 -- IMPORTANT -> Add the name of your server slot booking file below. Path starts in SavedGames/DCS.openbeta/
 local TOPSlotsFilePath = lfs.writedir() .. [[ServerBookings.json]]
-local TOPLogbookFilePath = lfs.writedir() .. [[Logbook.json]]
+-- local TOPLogbookFilePath = lfs.writedir() .. [[Logbook.json]]
+local TOPLogbookFilePath = "C:\\BTI\\Tracking\\Logbook.json"
 
 if JSONLib == nil then
     JSONLib = dofile("C:\\BTI\\Json.lua")
@@ -12,6 +13,27 @@ end
 local slotBooking = {}
 local slotRules = {}
 
+--- Business Logic utils --------------------------------------------------------------------------------------------
+local function basePlayerData()
+    return {
+        ["flights"] = {},
+        ["currentTakeoffTime"] = 0,
+        ["currentFriendlyFire"] = 0,
+        ["vehicles"] = 0,
+        ["planes"] = 0,
+        ["ships"] = 0,
+        ["score"] = 0,
+    }
+end
+
+local function getPlayerStat(playerID, statID)
+    local stat = net.get_stat(playerID, statID)
+    net.log("TOP: Player " .. playerID .. " STAT " .. statID  .. " value " .. tostring(stat))
+    if stat == nil then
+        return 0
+    end
+    return stat
+end
 --- CALLBACKS -------------------------------------------------------------------------------------------------------
 local TOPSlotsCallbacks = {}
 
@@ -68,35 +90,42 @@ end
 
 local LogbookTable = {}
 
-TOPSlotsCallbacks.onGameEvent = function(eventName, playerID, slotID)
-    if eventName ~= 'takeoff' and eventName ~= 'self_kill' and eventName ~= 'eject' and eventName ~= 'landing' and eventName ~= 'pilot_death' then
+TOPSlotsCallbacks.onGameEvent = function(eventName, playerID, arg3, arg2)
+    if
+        eventName ~= "takeoff" and eventName ~= "self_kill" and eventName ~= "eject" and eventName ~= "landing" and
+            eventName ~= "pilot_death" and eventName ~= "friendly_fire"
+     then
         return -- Return right away if we don't care about such event
     end
-    net.log("TOP: onGameEvent " .. eventName)
+    -- net.log("TOP: onGameEvent " .. eventName)
 
     local playerInfo = net.get_player_info(playerID)
     local playerUCID = playerInfo["ucid"]
-    net.log("TOP: onGameEvent for player ucid " .. playerUCID)
+    -- net.log("TOP: onGameEvent for player ucid " .. playerUCID)
     local playerData = LogbookTable[playerUCID]
-    if playerData == nil then
-        playerData = {
-            ["flights"] = {},
-            ["currentTakeoffTime"] = 0
-        }
+    if playerData == nil then -- Create playerData for the UCID if it doesn't exist yet
+        playerData = basePlayerData() -- Zero-ed out player score tracking and flights array
     end
 
-    if eventName == 'takeoff' then
+    if eventName == "takeoff" then
         playerData["currentTakeoffTime"] = os.time()
-        net.log("TOP: Setting currentTakeoffTime to " .. tostring(os.time()))
-    elseif eventName == 'self_kill' or eventName == 'eject' or eventName == 'landing' or eventName == 'pilot_death' then
+        net.log("TOP: Setting currentTakeoffTime to " .. tostring(os.time() .. " for " .. playerID))
+    elseif eventName == "self_kill" or eventName == "eject" or eventName == "landing" or eventName == "pilot_death" then
         if playerData["currentTakeoffTime"] == 0 then
             net.log("TOP: Logbook event arriving before takeoff event")
         end
 
+        local slotID = arg3
         local slotName = DCS.getUnitProperty(slotID, DCS.UNIT_NAME)
         local slotType = DCS.getUnitProperty(slotID, DCS.UNIT_TYPE)
         local landingTime = os.time()
+        local vehicles = getPlayerStat(playerID, net.PS_CAR)
+        local ships = getPlayerStat(playerID, net.PS_SHIP)
+        local planes = getPlayerStat(playerID, net.PS_PLANE)
+        local score = getPlayerStat(playerID, net.PS_SCORE)
+
         net.log("TOP: end flight event " .. eventName .. " slotName " .. slotName .. " slotType " .. slotType)
+        -- net.log("TOP: stats: score " .. tostring(score) .. " vehicles " .. tostring(vehicles) .. " planes " .. tostring(planes) .. " ships" .. tostring(ships) .. " teamkill " .. tostring(playerData["currentFriendlyFire"]))
         local flight = {
             ["id"] = tostring(playerData["currentTakeoffTime"]) .. tostring(landingTime),
             ["takeoffTime"] = playerData["currentTakeoffTime"],
@@ -104,23 +133,33 @@ TOPSlotsCallbacks.onGameEvent = function(eventName, playerID, slotID)
             ["slotName"] = slotName,
             ["slotType"] = slotType,
             ["outcome"] = eventName,
-            -- Add score here when figured out what teamkilling looks like
+            ["vehicles"] = vehicles - playerData["vehicles"],
+            ["planes"] = planes - playerData["planes"],
+            ["ships"] = ships - playerData["ships"],
+            ["score"] = score - playerData["score"],
+            ["friendlyFire"] = playerData["currentFriendlyFire"]
         }
-        net.log("TOP: After flight table ready " .. flight["id"])
-        table.insert( playerData["flights"], flight )
-        net.log("TOP: after insert " .. tostring(#playerData["flights"]))
-        playerData["currentTakeoffTime"] = 0 --Maybe we don't need that
+        -- net.log("TOP: After flight table ready " .. flight["id"])
+        table.insert(playerData["flights"], flight)
+        -- net.log("TOP: after insert " .. tostring(#playerData["flights"]))
+
+        -- Saving data to compare for next flight in the session
+        playerData["vehicles"] = vehicles
+        playerData["planes"] = planes
+        playerData["ships"] = ships
+        playerData["score"] = score
+        playerData["currentTakeoffTime"] = 0 -- resetting this as a flag
+        playerData["currentFriendlyFire"] = 0
         TOPsaveLogbookFile()
         net.log("TOP: Logbook file saved")
-    elseif eventName == 'kill' then
+    elseif eventName == "friendly_fire" then
+        playerData["currentFriendlyFire"] = playerData["currentFriendlyFire"] + 1
         -- check if player gets killed
-    elseif eventName == 'disconnect' then
-        -- trim data table maybe
     end
 
     LogbookTable[playerUCID] = playerData
-    net.log("TOP: end table takeofftime " .. tostring(LogbookTable[playerUCID]["currentTakeoffTime"]))
-    TOPsaveLogbookFile() -- remove that
+    -- net.log("TOP: end table takeofftime " .. tostring(LogbookTable[playerUCID]["currentTakeoffTime"]))
+    -- TOPsaveLogbookFile() -- remove that
     return --Return nothing to let other hooks take over
 end
 
